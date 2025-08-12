b8ed28e82e9d5d9929b831ba0b4b0bef46ebd84a
ZEN8890 <Steven.gunawan8@hotmail.com>
2025-08-12 10:35:03 +0700

    logo change

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -148,7 +148,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Strata Lite')),
+      appBar: AppBar(title: const Text('Login')),
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(20.0),
